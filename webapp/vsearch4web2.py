af9a14e8cefb30a4cd105ceaf057379ff1ddd7a6
anilhoon <anilhoon@gmail.com>
2019-11-22 09:44:36 +0900

    update

--- webapp/vsearch4web2.py
+++ webapp/vsearch4web2.py
@@ -5,7 +5,11 @@ app = Flask(__name__)
 
 def log_request(req: 'flask_request', res: str) -> None:
 	with open('vsearch.log', 'a') as log:
-		print(req, res, file=log)
+		print(req.form, req.remote_addr, req.user_agent, res, file=log, sep='|')
+		#print(req.form, file=log, end='|')
+		#print(req.remote_addr, file=log, end='|')
+		#print(req.user_agent, file=log, end='|')
+		#print(res, file=log)
 
 @app.route('/search4', methods=['POST'])
 def do_search() -> 'html':
@@ -20,10 +24,19 @@ def do_search() -> 'html':
                            the_results= results, )
                             
 @app.route('/viewlog')
-def view_the_log() -> str:
-	with open('vsearch.log') as log:
-		contents = log.read()
-	return escape(contents)
+def view_the_log() -> 'html':
+	contents=[]
+	with open('vsearch.log' ) as log:
+		for line in log:
+			contents.append([])
+			for item in line.split('|'):
+				contents[-1].append(escape(item))
+	titles = ('Form Data', 'Remote_addr', 'User_agent', 'Results')				
+	return render_template('viewlog.html' ,
+											the_title='View Log' ,
+											the_row_titles=titles,
+											the_data=contents,)
+
 	                           
 
     # return str(vsearch.search4letters(phrase, letters))
